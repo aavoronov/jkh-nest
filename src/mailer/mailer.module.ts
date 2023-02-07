357f8ac31de505487d1a3a03d6d59b7ba4e695a5
whatever <voronov.xcvi@gmail.com>
2023-02-07 11:19:53 +0300

    fixes

--- src/mailer/mailer.module.ts
+++ src/mailer/mailer.module.ts
@@ -1,11 +1,11 @@
-import { Module } from '@nestjs/common'
-import { MailerController } from './mailer.controller'
-import { MailerService } from './mailer.service'
+import { Module } from '@nestjs/common';
+import { MailerController } from './mailer.controller';
+import { MailerService } from './mailer.service';
 
 @Module({
-    controllers: [MailerController],
-    providers: [MailerService],
-    imports: [],
-    exports: [MailerService],
+  controllers: [MailerController],
+  providers: [MailerService],
+  imports: [],
+  exports: [MailerService],
 })
 export class MailerModule {}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,27 +1,27 @@
-import { MiddlewareConsumer, Module, RequestMethod } from "@nestjs/common";
-import { UsersService } from "./users.service";
-import { UsersController } from "./users.controller";
-import { AuthMiddleware } from "../utils/middleware/auth.middleware";
-import { AccessMiddleware } from "../utils/middleware/access.middleware";
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+import { AuthMiddleware } from '../utils/middleware/auth.middleware';
+import { AccessMiddleware } from '../utils/middleware/access.middleware';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
 })
 export class UsersModule {
-  // configure(consumer: MiddlewareConsumer) {
-  //   consumer
-  //     .apply(AuthMiddleware)
-  //     .forRoutes({ path: '/', method: RequestMethod.ALL });
-  //   consumer
-  //     .apply(AccessMiddleware)
-  //     .exclude(
-  //       { path: '', method: RequestMethod.POST },
-  //       { path: 'auth', method: RequestMethod.POST },
-  //       { path: 'confirm', method: RequestMethod.GET },
-  //       { path: 'restore', method: RequestMethod.POST },
-  //       { path: 'reauth', method: RequestMethod.GET },
-  //     )
-  //     .forRoutes({ path: '/', method: RequestMethod.ALL });
-  // }
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AuthMiddleware)
+      .forRoutes({ path: '/', method: RequestMethod.ALL });
+    consumer
+      .apply(AccessMiddleware)
+      .exclude(
+        { path: '', method: RequestMethod.POST },
+        { path: 'auth', method: RequestMethod.POST },
+        { path: 'confirm', method: RequestMethod.GET },
+        { path: 'restore', method: RequestMethod.POST },
+        { path: 'reauth', method: RequestMethod.GET },
+      )
+      .forRoutes({ path: '/', method: RequestMethod.ALL });
+  }
 }
