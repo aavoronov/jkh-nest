37ea9fa8b5501c5a5f45c97844561e9abc55576f
whatever <voronov.xcvi@gmail.com>
2023-05-22 16:58:26 +0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,19 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { SocketIOAdapter } from './socket-io-adapter';
+import { readFileSync } from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+    httpsOptions: {
+      key: readFileSync(`${process.env.LETSENCRYPT_DIR}/privkey.pem`),
+      cert: readFileSync(`${process.env.LETSENCRYPT_DIR}/cert.pem`),
+      ca: readFileSync(`${process.env.LETSENCRYPT_DIR}/chain.pem`),
+      requestCert: false,
+      rejectUnauthorized: false,
+    },
+  });
   app.setGlobalPrefix('api/v1');
   const configService = app.get(ConfigService);
   app.useWebSocketAdapter(new SocketIOAdapter(app, configService));
